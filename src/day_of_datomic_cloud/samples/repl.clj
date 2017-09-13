ba3c94ec1e4d55ebf6049acc17a8a24b72ad21d2
Marshall Thompson <marshall@cognitect.com>
2017-09-13 12:43:22 -0400

    Adding resources and config files

---
+++ src/day_of_datomic_cloud/samples/repl.clj
@@ -0,0 +1,198 @@
+;   Copyright (c) Cognitect, Inc. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns datomic.samples.repl
+  (:require [clojure.data.generators :as gen]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [datomic.client.api.alpha :as d])
+  (:import java.util.Random
+           (java.util UUID)))
+
+(defn scratch-db-conn
+  "Create a connection to a scratch database"
+  []
+  (let [client (d/client datomic-client-config)
+        db-name (str scratch-db-prefix (UUID/randomUUID))]
+    (d/delete-database client {:db-name db-name})
+    (d/create-database client {:db-name db-name})
+    (d/connect client {:db-name db-name})))
+
+(defn delete-scratch-dbs
+  []
+  "Deletes all DBs with the prefix \"dod-scratch-\""
+  (let [client (d/client datomic-client-config)
+        db-names (d/list-databases client {})]
+    (doseq [db-name db-names
+            :when (.startsWith db-name scratch-db-prefix)]
+      (println "Deleting DB: " db-name)
+      (d/delete-database client {:db-name db-name}))))
+
+(defn- read-one
+  [r]
+  (try
+    (read r)
+    (catch java.lang.RuntimeException e
+      (if (= "EOF while reading" (.getMessage e))
+        ::EOF
+        (throw e)))))
+
+(defn read-all
+  "Reads a sequence of top-level objects in file"
+  ;; Modified from Clojure Cookbook, L Vanderhart & R. Neufeld
+  [src]
+  (with-open [r (java.io.PushbackReader. (clojure.java.io/reader src))]
+    (binding [*read-eval* false]
+      (doall (take-while #(not= ::EOF %) (repeatedly #(read-one r)))))))
+
+(defn transact-all
+  "Load and run all transactions from f, where f is any
+   resource that can be opened by io/reader."
+  [conn f]
+  (loop [n 0
+         [tx & more] (read-all f)]
+    (if tx
+      (recur (+ n (count (:tx-data (d/transact conn {:tx-data tx}))))
+             more)
+      {:datoms n})))
+
+(defn transcript
+  "Run all forms, printing a transcript as if forms were
+   individually entered interactively at the REPL."
+  [forms]
+  (binding [*ns* *ns*]
+    (let [temp (gensym)]
+      (println ";; Executing forms in temp namespace: " temp)
+      (in-ns temp)
+      (clojure.core/use 'clojure.core 'clojure.repl 'clojure.pprint)
+      (doseq [f forms]
+        (pp/pprint f)
+        (print "=> ")
+        (pp/pprint (eval f))
+        (println))
+      (remove-ns temp)
+      :done)))
+
+(defmacro should-throw
+  "Runs forms, expecting an exception. Prints descriptive message if
+   an exception occurred. Throws if an exception did *not* occur."
+  [& forms]
+  `(try
+    ~@forms
+    (throw (ex-info "Expected exception" {:forms '~forms}))
+    (catch Throwable t#
+      (println "Got expected exception:\n\t" (.getMessage t#)))))
+
+(defn modes
+  "Returns the set of modes for a collection."
+  [coll]
+  (->> (frequencies coll)
+       (reduce
+        (fn [[modes ct] [k v]]
+          (cond
+           (< v ct)  [modes ct]
+           (= v ct)  [(conj modes k) ct]
+           (> v ct) [#{k} v]))
+        [#{} 2])
+       first))
+
+(defn generate-some-comments
+  "Generates transaction data for some comments"
+  [conn db n]
+  (let [story-ids (->> (d/q conn {:query '[:find ?e
+                                           :where [?e :story/url]]
+                                  :args [db]})
+                       (mapv first))
+        user-ids (->> (d/q conn {:query '[:find ?e
+                                          :where [?e :user/email]]
+                                 :args [db]})
+                      (mapv first))
+        comment-ids (->> (d/q conn {:query '[:find ?e
+                                             :where [?e :comment/author]]
+                                    :args [db]})
+                         (mapv first))
+        choose1 (fn [n] (when (seq n) (gen/rand-nth n)))]
+    (assert (seq story-ids))
+    (assert (seq user-ids))
+    (->> (fn []
+           (let [comment-id (str (UUID/randomUUID))
+                 parent-id (or (choose1 comment-ids) (choose1 story-ids))]
+             [[:db/add parent-id :comments comment-id]
+              [:db/add comment-id :comment/author (choose1 user-ids)]
+              [:db/add comment-id :comment/body "blah"]]))
+         (repeatedly n)
+         (mapcat identity))))
+;
+(defn setup-sample-db-1
+  [conn]
+  (doseq [schema ["day-of-datomic/social-news.edn"
+                  "day-of-datomic/provenance.edn"]]
+    (->> (io/resource schema)
+         (transact-all conn)))
+  (let [[[ed]] (seq (d/q conn {:query '[:find ?e
+                                        :where [?e :user/email "editor@example.com"]]
+                               :args [(d/db conn)]}))]
+    (d/transact conn {:tx-data [[:db/add ed :user/firstName "Edward"]]}))
+  (binding [gen/*rnd* (Random. 42)]
+    (dotimes [_ 4]
+      (d/transact conn {:tx-data (generate-some-comments conn (d/db conn) 5)}))
+    conn))
+
+(defn choose-some
+  "Pick zero or more items at random from a collection"
+  [coll]
+  (take (gen/uniform 0 (count coll))
+        (gen/shuffle coll)))
+
+(defn gen-users-with-upvotes
+  "Make transaction data for example users, possibly with upvotes"
+  [stories email-prefix n]
+  (mapcat
+   (fn [n]
+     (let [upvotes (map (fn [story] [:db/add "new-user" :user/upVotes story])
+                        (choose-some stories))]
+       (conj upvotes {:user/email (str email-prefix "-" n "@example.com")})))
+   (range n)))
+
+(defn trunc
+  "Return a string rep of x, shortened to n chars or less"
+  [x n]
+  (let [s (str x)]
+    (if (<= (count s) n)
+      s
+      (str (subs s 0 (- n 3)) "..."))))
+
+(def tx-part-e-a-added
+  "Sort datoms by tx, then e, then a, then added"
+  (reify
+   java.util.Comparator
+   (compare
+    [_ x y]
+    (cond
+     (< (:tx x) (:tx y)) -1
+     (> (:tx x) (:tx y)) 1
+     (< (:e x) (:e y)) -1
+     (> (:e x) (:e y)) 1
+     (< (:a x) (:a y)) -1
+     (> (:a x) (:a y)) 1
+     (false? (:added x)) -1
+     :default 1))))
+
+(defn datom-table
+  "Print a collection of datoms in an org-mode compatible table."
+  [db datoms]
+  (->> datoms
+       (map
+        (fn [{:keys [e a v tx added]}]
+          {"e" (format "0x%016x" e)
+           "a" (:db/ident (d/pull db {:selector '[:db/ident] :eid a}))
+           "v" (trunc v 24)
+           "tx" (format "0x%x" tx)
+           "added" added}))
+       (pp/print-table ["e" "a" "v" "tx" "added"])))
+
---
+++ src/day_of_datomic_cloud/samples/schema.clj
@@ -0,0 +1,72 @@
+(ns datomic.samples.schema
+  (:require [datomic.client.api.alpha :as d]))
+
+(defn cardinality
+  "Returns the cardinality (:db.cardinality/one or
+   :db.cardinality/many) of the attribute"
+  [db attr]
+  (-> (d/pull db
+              {:selector '[:db/cardinality]
+               :eid attr})
+      (get-in [:db/cardinality :db/ident])))
+
+(defn has-attribute?
+  "Does database have an attribute named attr-name?"
+  [db attr-name]
+  (-> (d/pull db
+              {:selector '[:db/valueType]
+               :eid attr-name})
+      seq
+      boolean))
+
+(defn has-schema?
+  "Does database have a schema named schema-name installed?
+   Uses schema-attr (an attribute of transactions!) to track
+   which schema names are installed."
+  [conn db schema-attr schema-name]
+  (and (has-attribute? db schema-attr)
+       (-> (d/q conn {:query '[:find ?e
+                               :in $ ?sa ?sn
+                               :where [?e ?sa ?sn]]
+                      :args [db schema-attr schema-name]})
+           seq
+           boolean)))
+
+(defn- ensure-schema-attribute
+  "Ensure that schema-attr, a keyword-valued attribute used
+   as a value on transactions to track named schemas, is
+   installed in database."
+  [conn schema-attr]
+  (when-not (has-attribute? (d/db conn) schema-attr)
+    (d/transact conn {:tx-data [{:db/ident schema-attr
+                                 :db/valueType :db.type/keyword
+                                 :db/cardinality :db.cardinality/one
+                                 :db/doc "Name of schema installed by this transaction"}]})))
+
+(defn ensure-schemas
+  "Ensure that schemas are installed.
+
+      schema-attr   a keyword valued attribute of a transaction,
+                    naming the schema
+      schema-map    a map from schema names to schema installation
+                    maps. A schema installation map contains two
+                    keys: :txes is the data to install, and :requires
+                    is a list of other schema names that must also
+                    be installed
+      schema-names  the names of schemas to install"
+  [conn schema-attr schema-map & schema-names]
+  (ensure-schema-attribute conn schema-attr)
+  (doseq [schema-name schema-names]
+    (when-not (has-schema? conn (d/db conn) schema-attr schema-name)
+      (let [{:keys [requires txes]} (get schema-map schema-name)]
+        (apply ensure-schemas conn schema-attr schema-map requires)
+        (if txes
+          (doseq [tx txes]
+            ;; hrm, could mark the last tx specially
+            (d/transact conn {:tx-data (cons {:db/id "datomic.tx"
+                                              schema-attr schema-name}
+                                             tx)}))
+          (throw (ex-info (str "No data provided for schema" schema-name)
+                          {:schema/missing schema-name})))))))
+
+
