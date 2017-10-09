5453079b608ac6e025c84afb0d5a19bbc9b53f3d
Timothy Baldridge <tbaldridge@gmail.com>
2017-10-09 16:24:44 -0600

    initial spike of fn-fx render for dodc repl ui

--- src/datomic/dodc/repl_ui_fx.clj
+++ src/datomic/dodc/repl_ui_fx.clj
@@ -3,42 +3,94 @@
    [clojure.spec.alpha :as s]
    [datomic.dodc.repl-ui.specs :as specs]
    [fn-fx.fx-dom :as dom]
+   [fn-fx.diff :refer [defui]]
    [fn-fx.controls :as ui]))
 
-(defn stage
-  [title]
-  (ui/stage :title title
-            :shown true
-            :min-width 300
-            :min-height 300))
+(defmulti render-data (fn [{:keys [spec]}]
+                        spec))
 
-(defonce dom-ref
-  (delay
-   (atom (dom/app (stage "REPL eval")))))
+(defmethod render-data :default
+  [{:keys [spec]}]
+  (ui/label :text (str "No Renderer for " spec)))
 
-;; TODO: how to get discover the options supported by the various controls?
-(def renderers-ref
+(defui Stage
+  (render [this {:keys [title] :as state}]
+          (ui/stage
+           :title title
+           :shown true
+           :min-width 600
+           :min-height 600
+           :scene (ui/scene
+                   :root (ui/stack-pane
+                          :children [(render-data state)])))))
+
+(def spec-ordering
   (atom
-   [[::specs/table identity]
-    [::specs/schema-by-ns identity]
-    [::specs/rectangle identity]]))
+   [::specs/table
+    ::specs/schema-by-ns
+    ::specs/rectangle]))
 
-(defn render
-  [x]
-  (reduce
-   (fn [_ [spec f]]
-     (when (s/valid? spec x)
-       (swap! @dom-ref dom/update-app (stage (str spec)))
-       (reduced spec)))
-   nil
-   @renderers-ref))
+(defn spec-for [x]
+  (or (some #(when (s/vlaid? % x)
+               %) @spec-ordering)
+      ::unknown))
+
+;;; RENDERERS
+
+(defmethod render-data ::specs/table
+  [{:keys [data]}]
+  (let [[column-defs rows] data]
+    (ui/table-view
+     ;:min-width 600
+     ;:min-height 600
+     :columns (for [column column-defs]
+                              (ui/table-column
+                               :text (pr-str column)
+                               :cell-value-factory (ui/map-value-factory :key column)))
+                   :items rows)))
+
+
+;;;
+
+(defn render [data-state data]
+  (let [spec (spec-for data)]
+    (println "SPEC " spec)
+    (send data-state assoc :spec spec :data data :title (str "REPL Eval: " spec))))
 
 (defn render-and-print
   "REPL printer that will render eval results into a JavaFX window,
 if they match a spec in renderers-ref. Use with e.g.
 
-   (main/repl :print ui/render-and-print)
+   (main/repl :print (ui/render-and-print))
 "
-  [x]
-  (render x)
-  (prn x))
+  []
+  (let [data-state (agent {:title "REPL Eval"
+                           :spec nil
+                           :data nil})
+        handler-fn (fn [event] nil) ;; No UI events for now
+        ui-state (agent (dom/app (stage @data-state) handler-fn))]
+    (add-watch data-state :ui (fn [_ _ _ _]
+                                (send ui-state
+                                      (fn [old-ui]
+                                        (dom/update-app old-ui (stage @data-state))))))
+    (fn [x]
+      (render data-state x)
+      (prn x))))
+
+
+(comment
+
+  (let [f (render-and-print)]
+    (dotimes [x 10]
+      (f [[:x :y] [{:x 1 :y 2} {:x x :y 3}]])
+      (Thread/sleep 1000))
+    (f "Hey"))
+
+  (spec-for [[:x :y] [{:x 1 :y 2} {:x 2 :y 3}]])
+
+
+
+  )
+
+
+
