c7a2b30275e9c38ba666987a13a8c97918167575
Marshall Thompson <marshall@cognitect.com>
2017-10-11 08:47:08 -0400

    project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject day-of-datomic-cloud "0.1.0-SNAPSHOT"
+  :description "Day of Datomic Cloud"
+  :source-paths ["src" "tutorial" "doc-examples"]
+  :resource-paths ["resources"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.datomic/client-cloud "0.8.42"]
+                 [seesaw/seesaw "1.4.5"]])
+
