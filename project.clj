fca9e08c5969e935fa1fd8f888dd80cd2caf77f9
Marshall Thompson <marshall@cognitect.com>
2019-04-24 15:39:16 -0400

    update versions

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Day of Datomic Cloud"
   :source-paths ["src" "tutorial" "doc-examples"]
   :resource-paths ["resources"]
-  :dependencies [[org.clojure/clojure "1.9.0-rc2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.datomic/client-cloud "0.8.46"]
+                 [com.datomic/client-cloud "0.8.71"]
                  [seesaw/seesaw "1.4.5"]])
 
