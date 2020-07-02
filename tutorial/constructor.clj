60c23856126e1da6fffd3d7ebaaf608289503591
Stuart Halloway <stu@cognitect.com>
2020-07-02 06:53:43 -0400

    use dev-local

--- tutorial/constructor.clj
+++ tutorial/constructor.clj
@@ -9,14 +9,10 @@
 ;  NOTE: You must run an Ion deploy of the day-of-datomic-cloud project to
 ;  your Datomic system prior to running these examples.
 
-(require
-  '[datomic.client.api :as d]
-  '[datomic.samples.repl :as repl])
-(import '(java.util UUID))
-
-(def client-cfg (read-string (slurp "config.edn")))
-(def client (d/client client-cfg))
-(def db-name (str "scratch-" (UUID/randomUUID)))
+(require '[datomic.client.api :as d]
+         '[datomic.samples.repl :as repl])
+(def client (d/client {:server-type :dev-local :system "day-of-datomic-cloud"}))
+(def db-name "constructor")
 (d/create-database client {:db-name db-name})
 (def conn (d/connect client {:db-name db-name}))
 
@@ -31,21 +27,21 @@
               :db.entity/attrs [:user/name :user/email]}])
 (d/transact conn {:tx-data schema})
 
-(def db (d/db conn))
-
 ;; valid user
-(d/with (d/with-db conn) {:tx-data [{:user/email "jdoe@example.com"
-                                     :user/name "John Doe"
-                                     :db/ensure :user/validate}]})
+(d/transact conn {:tx-data [{:user/email "jdoe@example.com"
+                             :user/name "John Doe"
+                             :db/ensure :user/validate}]})
 
 ;; not valid -- no email
-(-> (d/with (d/with-db conn) {:tx-data [{:user/name "John Doe"
-                                         :db/ensure :user/validate}]})
+(-> (d/transact conn {:tx-data [{:user/name "John Doe"
+                                 :db/ensure :user/validate}]})
     repl/thrown-data)
 
 ;; not valid -- too long
-(-> (d/with (d/with-db conn) {:tx-data [{:user/name "This user name is too long"
-                                         :db/ensure :user/validate}]})
+(-> (d/transact conn {:tx-data [{:user/name "This user name is too long"
+                                 :db/ensure :user/validate}]})
     repl/thrown-data)
 
-(d/delete-database client {:db-name db-name})
+
+
+
